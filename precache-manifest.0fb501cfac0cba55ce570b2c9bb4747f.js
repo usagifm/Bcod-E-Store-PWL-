579b0b2c033be0e86da863eb3185372a16581aa3
usagifm <44514120+usagifm@users.noreply.github.com>
2020-05-09 16:16:12 +0700

    Updates

---
+++ precache-manifest.0fb501cfac0cba55ce570b2c9bb4747f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472bf3cf3c10fccddabbee41d7835b4c",
+    "url": "/Bcod-E-Store-PWL-/index.html"
+  },
+  {
+    "revision": "be7c4d142f09b8dc6c85",
+    "url": "/Bcod-E-Store-PWL-/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "1fb89dfe5e1a71407db8",
+    "url": "/Bcod-E-Store-PWL-/static/css/main.d6c2332f.chunk.css"
+  },
+  {
+    "revision": "be7c4d142f09b8dc6c85",
+    "url": "/Bcod-E-Store-PWL-/static/js/2.f61b8fff.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/Bcod-E-Store-PWL-/static/js/2.f61b8fff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb89dfe5e1a71407db8",
+    "url": "/Bcod-E-Store-PWL-/static/js/main.893a9903.chunk.js"
+  },
+  {
+    "revision": "8713416bc86fe66cf622",
+    "url": "/Bcod-E-Store-PWL-/static/js/runtime-main.ff26a691.js"
+  }
+]);
