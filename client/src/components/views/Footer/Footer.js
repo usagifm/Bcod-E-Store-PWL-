48a087ea193567e718e140cadd0cf4f86137c2ca
affandi99 <50167162+affandi99@users.noreply.github.com>
2020-05-14 03:26:37 +0700

    memperbaiki layout footer

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -2,26 +2,31 @@ import React from 'react'
 import {Icon} from 'antd';
 import './style.css';
 
+
 function Footer() {
     return (
         <nav className="footer" >
+            <div className="container">
          <div className="box">
          <a href="https://play.google.com/store/apps">
                  <p>Download Aplikasi Mobile Bcod-EStore</p>
                  
             </a>
              </div>
-             
+
              <div className="box">
-             <a href="">
-                 <p>Tim Pengembang : BcodEv</p>
-                 </a>
+                 <p>Tim Pengembang</p>
+                 <img src="assets/facebook.PNG" width="49" ></img>
+                 
              </div>
 
              <div className="box">
-             <a href="">
-                 <p>Tata cara Pembelian\n hehe</p>
-                 </a>
+                 <p>Lebih Dekat dengan Kami!!</p>
+                 <img src="assets/facebook.PNG" width="49" ></img>
+                 <img src="assets/Instagram_icon.PNG" width="49" ></img>
+               
+
+             </div>
              </div>
          </nav>
     )
