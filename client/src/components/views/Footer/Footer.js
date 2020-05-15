f271f087688fa20d0fcbb3319fdc5fb672adeb25
affandi99 <50167162+affandi99@users.noreply.github.com>
2020-05-16 01:05:37 +0700

    fix in footer dan navbar

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -9,7 +9,10 @@ function Footer() {
             <div className="container">
          <div className="box">
         <p>Download Aplikasi Mobile Bcod-EStore</p>
-         
+         <div className="menu_1">
+        <a href="https://play.google.com/store/apps">
+            <img style={{ width: '100px', height:'auto' }} src={require('../../promoimg/google.png')} alt="Logo"/></a>
+      </div>
              </div>
 
              <div className="box">
@@ -23,7 +26,9 @@ function Footer() {
              <div className="box">
                  <p>Lebih Dekat dengan Kami!!</p>
                  <div className="menu_2">
-       
+        <a href="https://www.instagram.com"><img style={{ width: '44px', height:'auto' }} src={require('../../promoimg/Instagram_icon.png')} alt="Logo"/></a>
+        <a href="https://web.facebook.com/"><img style={{ width: '50px', height:'auto' }} src={require('../../promoimg/facebook.png')} alt="Logo"/></a>
+        <a href="https://twitter.com/home"><img style={{ width: '43px', height:'auto' }} src={require('../../promoimg/twitt.png')} alt="Logo"/></a>
       </div>
   
 
