72bc3e92f7e3659ba7c922cfd5fcb880e595f203
Abbi Kurnia <requiemzerophantasm@gmail.com>
2020-05-15 22:35:14 +0700

    Update Landingpage Promo & Navbar Logo
    Landingpage.js
    Navbar.js

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -164,13 +164,13 @@ function LandingPage() {
             }} >
                 <Slider { ...settingsHeader }>
                   <div>
-                    <img style={{ width: '100%', height:'220px' }} src={require('../../promoimg/1589397439149_1427472_1.jpg')} alt="Photo 1"/>
+                    <img style={{ width: '100%', height:'200px' }} src={require('../../promoimg/grandopening.jpg')} alt="Photo 1"/>
                   </div>
                   <div>
-                    <img style={{ width: '100%', height:'220px' }} src={require('../../promoimg/1589396167756_IMG-20200419-WA0011.jpg')} alt="Photo 2"/>
+                    <img style={{ width: '100%', height:'200px' }} src={require('../../promoimg/promolebaran.jpg')} alt="Photo 2"/>
                   </div>
                   <div>
-                    <img style={{ width: '100%', height:'220px' }} src={require('../../promoimg/1589396162829_IMG-20200419-WA0012.jpg')} alt="Photo 3"/>
+                    <img style={{ width: '100%', height:'200px' }} src={require('../../promoimg/Social.jpg')} alt="Photo 3"/>
                   </div>
                 </Slider>
             </div>
